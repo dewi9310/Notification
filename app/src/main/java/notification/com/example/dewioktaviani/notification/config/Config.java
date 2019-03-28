72ab2cacb5073f443c3706ba0fc4ba45eded8310
dewi9310 <dewioktaviani9310@gmail.com>
2019-03-28 17:35:17 +0700

    Initial commit

---
+++ app/src/main/java/notification/com/example/dewioktaviani/notification/config/Config.java
@@ -0,0 +1,12 @@
+package notification.com.example.dewioktaviani.notification.config;
+
+/**
+ * Created by dewi.oktaviani on 27/03/2019.
+ */
+
+public class Config {
+    public static String content = "";
+    public static String title = "";
+    public static String imageUrl = "";
+    public static String gameUrl = "";
+}
